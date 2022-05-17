53ea6d24170394ea024c0a9456c834c1e850c243
hhlwt <from.module.import@gmail.com>
2022-05-17 09:52:04 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -7,25 +7,21 @@ const playGame = (mission, generateRound) => {
   console.log(mission);
 
   const countOfRounds = 3;
-  let correctAnswersCount = 0;
 
   for (let round = 1; round <= countOfRounds; round += 1) {
     const [question, correctAnswer] = generateRound();
     const userAnswer = readlineSync.question(`Question: ${question}\nYour answer: `);
     if (userAnswer === correctAnswer) {
       console.log('Correct!');
-      correctAnswersCount += 1;
     } else {
       console.log(
         `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`,
       );
       console.log(`Let's try again, ${name}!`);
-      break;
+      return;
     }
   }
-  if (correctAnswersCount === 3) {
-    console.log(`Congratulations, ${name}!`);
-  }
+  console.log(`Congratulations, ${name}!`);
 };
 
 export default playGame;
